cc8facf10d8b903dca8af7a86a982da85e88156a
Kevin Messiaen <kmessiaen@nextoo.fr>
2019-12-27 10:16:24 +0100

    initial commit

---
+++ src/test/java/fr/nextoo/micro/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nextoo.micro.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
